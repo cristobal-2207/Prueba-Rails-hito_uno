0f490be9755bcad2b45a2bfefb9446f7e018396a
cristobal-2207 <cristobalgutierreznavarro@gmail.com>
2020-08-29 22:03:31 -0400

    Se genera el CRUD para Tweet, prueba la creación y ver un tweet, se reconoce el id del usuario.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  
   def index
+    @tweets = Tweet.order("created_at DESC").page params[:page]
+    @tweet = Tweet.new
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  get 'pages/index'
   devise_for :users
+  root 'tweets#index'
+  resources :tweets do
+    post 'likes', to: 'tweets#likes'
+    post 'retweet', to: 'tweets#retweet'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
